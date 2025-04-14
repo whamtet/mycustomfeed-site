eb1253dd7e60ae48cc673d5bf5fb88921d74dd56
Matthew Molloy <whamtet@gmail.com>
2025-04-15 03:47:36 +0900

    v1 copy

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,14 @@ module.exports = {
       ...colors,
       'clj-green-light': '#A2DA5F',
       'clj-blue-light': '#97B3F8',
-      'clj-blue': '#6180D2'
+      'clj-blue': '#6180D2',
+      'dark': '#2B2539',
+      'pale-yellow': '#FFFEF7',
+      'light-blue': '#B9ECFF',
+      'light-red': '#FFB9B9',
+      'th-gray': '#F0F0F0',
+      'td-gray': '#FFFFFF',
+      'td-border-gray': '#E5E5E5',
     }
   },
   plugins: [],
